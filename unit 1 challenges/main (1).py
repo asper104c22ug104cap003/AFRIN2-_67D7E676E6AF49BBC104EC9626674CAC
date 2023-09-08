42c1abbb6ea4c62143a92f577c3eb7a20fd9bb4c
asper104c22ug104cap003 <144420870+asper104c22ug104cap003@users.noreply.github.com>
2023-09-07 23:49:50 -0700

    Add files via upload

---
+++ unit 1 challenges/main (1).py
@@ -0,0 +1,12 @@
+#1.1 Implement a recursive function to calculate the factorial of a given number.
+
+def fact(n):
+  """This is a recursive function to find the factorial of an integer"""
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * fact(n-1)
+
+number = 5
+res = fact(number)
+print("The factorial of {} is {}.".format(number,res))
---
+++ unit 1 challenges/main (2).py
@@ -0,0 +1,14 @@
+#leap year
+year = int(input("Enter year to be checked: "))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("the year is a leap year!")
+    else:
+      print("The year is not a leap year!")
+  else:
+    print("The year is a leap year!")
+else:
+  print("The year is not a leap year!")
+  
