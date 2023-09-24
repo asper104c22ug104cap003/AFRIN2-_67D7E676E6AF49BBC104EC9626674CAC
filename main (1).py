743c425f288ebe246b7bbb2d9f84453048912a32
asper104c22ug104cap003 <144420870+asper104c22ug104cap003@users.noreply.github.com>
2023-09-24 11:19:30 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,44 +1,29 @@
-def sort_students(student_list):
-
-    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
-
-    return sorted_students
-
-
-class Student:
-
-    def __init__(self, name, roll_number, cgpa):
-
-        self.name = name
-
-        self.roll_number = roll_number
-
-        self.cgpa = cgpa
+'''Implement a class called Player that represents a cricket player. The Player class should have a
+method called play() which prints "The player is playing cricket. Derive two classes, Batsman and
+Bowler, from the Player class. Override the play() method in each derived class to print "The batsman
+is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the
+Batsman and Bowler classes and call the play() method for each object.'''
 
 
+# Define the base class Player
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
 
-# Test with different input lists of students
+# Define the derived class Batsman
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
 
-students_list = [
+# Define the derived class Bowler
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
 
-    Student("John", "2021001", 3.9),
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
 
-    Student("Jane", "2021002", 3.7),
-
-    Student("Alice", "2021003", 3.8),
-
-    Student("Bob", "2021004", 3.6)
-
-]
-
-
-
-sorted_students = sort_students(students_list)
-
-
-
-# Print the sorted list of students
-
-for student in sorted_students:
-
-    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
+# Call the play() method for each object
+batsman.play()
+bowler.play()
--- main.py
+++ main.py
@@ -1,32 +1,70 @@
-def linear_search_product(products, target_product):
+class BankAccount:
 
-    indices = []
+    def __init__(self, account_number, account_holder_name, initial_balance=0):
 
-    for index, product in enumerate(products):
+        self.__account_number = account_number
 
-        if product == target_product:
+        self.__account_holder_name = account_holder_name
 
-            indices.append(index)
-
-    return indices
-# Sample list of products
-
-product_list = ["apple", "banana", "orange", "apple", "grape", "apple"]
+        self.__account_balance = initial_balance
 
 
 
-# Target product to search for
+    def deposit(self, amount):
 
-target_product = "apple"
+        if amount > 0:
+
+            self.__account_balance += amount
+
+            print(f"Deposited {amount} units. New balance: {self.__account_balance}")
+
+        else:
+
+            print("Deposit amount must be greater than zero.")
 
 
 
-# Call the function
+    def withdraw(self, amount):
 
-result = linear_search_product(product_list, target_product)
+        if 0 < amount <= self.__account_balance:
+         self.__account_balance -= amount
+
+         print(f"Withdrew {amount} units. New balance: {self.__account_balance}")
+
+        else:
+
+            print("Withdrawal amount must be greater than zero and less than or equal to the account balance.")
 
 
 
-# Print the result
+    def display_balance(self):
 
-print(result)
+        print(f"Account Holder: {self.__account_holder_name}")
+
+        print(f"Account Number: {self.__account_number}")
+
+        print(f"Account Balance: {self.__account_balance}")
+
+
+
+
+
+# Testing the BankAccount class
+
+if __name__ == "__main__":
+
+    account = BankAccount("1234567890", "John Doe", 1000)
+
+
+
+    account.display_balance()  # Should display the initial balance
+
+
+
+    account.deposit(500)  # Depositing 500 units
+
+    account.withdraw(200)  # Withdrawing 200 units
+
+
+
+    account.display_balance()  # Should display the updated balance after transactions
